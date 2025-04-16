e09943dc0d63bba18e687a53e2c854df6344931a
Okhan-Coder <O_khan4@u.pacific.edu>
2025-04-15 20:57:46 -0700

    Added FPS to Game

--- main.py
+++ main.py
@@ -4,13 +4,16 @@ def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT} ")
-    Continue = True
+    conti = True
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
-    while Continue:
+    game_time = pygame.time.Clock()
+    dt = 0
+    while conti:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = game_time.tick(60)/1000
 if __name__ == "__main__":
     main()
