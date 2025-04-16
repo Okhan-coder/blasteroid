7a2c2f1d8a6ee59572eb5751b59dc7361edf2334
Okhan-Coder <O_khan4@u.pacific.edu>
2025-04-15 22:47:32 -0700

    added Player + Shape  class and Player to Screen

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from constants import *
 import pygame
+from player import Player
 def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT} ")
     conti = True
+    x=int(SCREEN_WIDTH/2)
+    y=int(SCREEN_HEIGHT/2)
+    print(type(x))
+    space_ship = Player(x,y)
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
     game_time = pygame.time.Clock()
     dt = 0
@@ -13,6 +18,7 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        space_ship.draw(screen)
         pygame.display.flip()
         dt = game_time.tick(60)/1000
 if __name__ == "__main__":
