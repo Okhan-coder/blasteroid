199b28860533bf1fb8874adb10b9ea5fc15b9de5
Okhan-Coder <O_khan4@u.pacific.edu>
2025-04-16 18:21:35 -0700

    added collision and Loss condition

--- circleshape.py
+++ circleshape.py
@@ -17,4 +17,9 @@ class CircleShape(pygame.sprite.Sprite):
      def update(self,dt):
         #Update Screen
         pass
-
+     def collide(self,circle):
+         distance = self.position.distance_to(circle.position)
+         r_distance = self.radius + circle.radius
+         if distance > r_distance:
+             return True
+         return False
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from constants import *
 import pygame
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -9,19 +11,35 @@ def main():
     x=int(SCREEN_WIDTH/2)
     y=int(SCREEN_HEIGHT/2)
     print(type(x))
-    space_ship = Player(x,y)
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
     game_time = pygame.time.Clock()
     dt = 0.0
+    # Insert objects into a group so DRY is maintained
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids,updatable,drawable)
+    AsteroidField.containers = (updatable)
+    print(drawable.sprites())    
+    space_ship = Player(x,y)
+    sp_asteroid = AsteroidField()
     while conti:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        space_ship.draw(screen)
-        space_ship.update(dt)
+        for obj in drawable:
+            obj.draw(screen)
+        for obj in updatable:
+            obj.update(dt)
+        for obj in asteroids:
+            conti = space_ship.collide(obj)
+            if not conti:
+              print("GAME OVER")
+              exit()
+            
         pygame.display.flip()
         dt = (game_time.tick(60))/1000
-
 if __name__ == "__main__":
     main()
